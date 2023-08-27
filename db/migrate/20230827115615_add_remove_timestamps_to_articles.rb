4f8e1c22eb80a4f44df2b709a18fd2e5a8842af4
PyCreatorr <ttptv@yahoo.com>
2023-08-27 14:05:12 +0200

    Add remove the colums in the migrate schema of db

---
+++ db/migrate/20230827115615_add_remove_timestamps_to_articles.rb
@@ -0,0 +1,5 @@
+class AddRemoveTimestampsToArticles < ActiveRecord::Migration[7.0]
+  def change
+    add_column :articles, :created_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_27_075321) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_27_115615) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "created_at"
   end
 
 end
