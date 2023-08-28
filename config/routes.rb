9c599131a4676ae9a1bd73e6c8efbcc8788ab953
PyCreatorr <ttptv@yahoo.com>
2023-08-28 10:37:31 +0200

    Add all articles show feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources :articles
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :articles, only: [:show, :index]
 
   root 'pages#home'
   get 'about', to: 'pages#about'
+
+
 end
