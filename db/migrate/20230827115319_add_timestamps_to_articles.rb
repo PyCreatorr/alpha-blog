d4567a2eca8a31850a40387b9229c6c89b8a5376
PyCreatorr <ttptv@yahoo.com>
2023-08-30 21:11:31 +0200

    Prepare for deploy

--- db/migrate/20230827115319_add_timestamps_to_articles.rb
+++ db/migrate/20230827115319_add_timestamps_to_articles.rb
@@ -1,6 +1,6 @@
 class AddTimestampsToArticles < ActiveRecord::Migration[7.0]
   def change
-    remove_column :articles, "created_at"
-    remove_column :articles, "updates_at"
+    #remove_column :articles, "created_at"
+    #remove_column :articles, "updates_at"
   end
 end
--- db/migrate/20230827115615_add_remove_timestamps_to_articles.rb
+++ db/migrate/20230827115615_add_remove_timestamps_to_articles.rb
@@ -1,5 +1,5 @@
 class AddRemoveTimestampsToArticles < ActiveRecord::Migration[7.0]
   def change
-    add_column :articles, :created_at, :datetime
+    #add_column :articles, :created_at, :datetime
   end
 end
