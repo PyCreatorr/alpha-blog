4094d2e4d1bb6a5bd4678b213da9a8d9d97f6692
PyCreatorr <ttptv@yahoo.com>
2023-09-04 17:07:12 +0200

    Add to the User model email downcase before_save method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     has_many :articles
     validates :username, presence: true,  uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
     VALID_EMAIL_REGEX= /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
