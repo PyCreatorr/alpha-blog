411cc2e4bd257e67ef60af803e9beaa261e421d5
PyCreatorr <ttptv@yahoo.com>
2023-09-16 09:39:23 +0200

    BACK-END Many-to-Many association: categories <> article_categories <> articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,10 @@
 class Article < ApplicationRecord
+    # association one-to-many    
     belongs_to :user
+    # association many-to-many through article_categories
+    has_many :article_categories 
+    has_many :categories, through: :article_categories
+    
     validates :title, presence: true, length: {minimum: 6, maximum: 100}
     validates :description, presence: true, length: {minimum: 10, maximum: 400}
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
+    # associaton many-to-many
+    has_many :article_categories
+    has_many :articles, through: :article_categories
+
     validates :name, presence: true#, uniqueness: { case_sensitive: false }
     validates_uniqueness_of :name
     validates :name, length: { minimum: 3, maximum: 25 }
